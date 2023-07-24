7ed01db7e1a1284c0002499304f3f5b02b5b011f
gvv <valerii.hubskyi@ekreative.com>
2023-07-24 17:46:23 +0300

    Add check: Controller should not expose mutable public property

--- lib/lifemd_mobile_custom_lint.dart
+++ lib/lifemd_mobile_custom_lint.dart
@@ -1,10 +1,11 @@
 // This is the entrypoint of our custom linter
 import 'package:custom_lint_builder/custom_lint_builder.dart';
-import 'package:lifemd_mobile_custom_lint/rules/equtable_public_property_documentation.dart';
+import 'package:lifemd_mobile_custom_lint/rules/controller_public_mutable_property.dart';
 
 import 'rules/avoid_top_level_members_in_tests.dart';
 import 'rules/controller_public_member_documentation.dart';
 import 'rules/controller_public_method_name.dart';
+import 'rules/equtable_public_property_documentation.dart';
 import 'rules/list_all_equatable_fields.dart';
 import 'rules/prefer_correct_test_file_name.dart';
 import 'rules/prefer_single_widget_per_file.dart';
@@ -19,7 +20,7 @@ class _MobileCustomLinter extends PluginBase {
         ControllerPublicMethodName(),
         ControllerPublicMemberDocumentation(),
         EquatablePublicPropertyDocumentation(),
-        // ControllerPublicMutableProperty(),
+        ControllerPublicMutableProperty(),
         PreferSingleWidgetPerFile(),
         PreferCorrectTestFileName(),
         ListAllEquatableFields(),
