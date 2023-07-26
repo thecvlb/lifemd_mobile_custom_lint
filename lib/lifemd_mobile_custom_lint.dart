7713e9bcc65fb50990e8ba5ac824c9cfb47f4657
gvv <valerii.hubskyi@ekreative.com>
2023-07-26 11:48:04 +0300

    add avoid-expanded-as-spacer and prefer-correct-edge-insets-constructor

--- lib/lifemd_mobile_custom_lint.dart
+++ lib/lifemd_mobile_custom_lint.dart
@@ -1,12 +1,14 @@
 // This is the entrypoint of our custom linter
 import 'package:custom_lint_builder/custom_lint_builder.dart';
-import 'package:lifemd_mobile_custom_lint/rules/controller_public_mutable_property.dart';
 
+import 'rules/avoid_expanded_as_spacer.dart';
 import 'rules/avoid_top_level_members_in_tests.dart';
 import 'rules/controller_public_member_documentation.dart';
 import 'rules/controller_public_method_name.dart';
+import 'rules/controller_public_mutable_property.dart';
 import 'rules/equtable_public_property_documentation.dart';
 import 'rules/list_all_equatable_fields.dart';
+import 'rules/prefer_correct_edge_insets_constructor.dart';
 import 'rules/prefer_correct_test_file_name.dart';
 import 'rules/prefer_single_widget_per_file.dart';
 
@@ -25,5 +27,7 @@ class _MobileCustomLinter extends PluginBase {
         PreferCorrectTestFileName(),
         ListAllEquatableFields(),
         AvoidTopLevelMembersInTests(),
+        AvoidExpandedAsSpacer(),
+        PreferCorrectEdgeInsetsConstructor(),
       ];
 }
